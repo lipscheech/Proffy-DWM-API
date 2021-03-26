960d389aff62d3e87f8d43b103b6e6e4d4ad4d20
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-03-26 11:14:03 -0300

    Changed the database and added instructions

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,9 @@
-require('dotenv').config()
 import path from 'path';
 
 module.exports = {
-  client: 'pg',
+  client: 'sqlite3',
   connection: {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
+    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,10 @@
 import knex from 'knex';
+import path from 'path';
 
 const db = knex({
-  client: 'pg',
+  client: 'sqlite3',
   connection: {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
+    filename: path.resolve(__dirname, 'database.sqlite'),
   },
   useNullAsDefault: true,
 });
